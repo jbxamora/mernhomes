76369dec4984e6bd8357e5eb54e728683f52bedf
jbxamora <jbxamora@icloud.com>
2023-03-25 17:22:33 -0700

    updated create property route logic

--- server/controllers/propertyController.js
+++ server/controllers/propertyController.js
@@ -64,42 +64,47 @@ const getPropertyDetail = async (req, res) => {
 };
 
 const createProperty = async (req, res) => {
+    try {
+        const {
+            title,
+            description,
+            propertyType,
+            location,
+            price,
+            photo,
+            email,
+        } = req.body;
 
-try {
-    const { title, description, propertyType, location, price, photo,
-    email } = req.body;
-    
-    const session = await mongoose.startSession();
-    session.startTransaction();
-    
-    const user = await User.findOne({ email }).session(session);
-    
-    if(!user) throw new Error('User not found')
-    
-    const photoUrl = await cloudinary.uploader.upload(photo);
-    
-    const newProperty = await Property.create({
-        title,
-        description,
-        propertyType,
-        location,
-        price,
-        photo: photoUrl.url,
-        creator: user._id
-    });
-    
-    user.allProperties.push(newProperty._id);
-    await user.save({ session });
-    
-    await session.commitTransaction();
-    
-    res.status(200).json({ message: 'Property created successfully' });;
+        const session = await mongoose.startSession();
+        session.startTransaction();
 
-} catch {
-    res.status(500).json({ message: error.message });
-}
+        const user = await User.findOne({ email }).session(session);
 
+        if (!user) throw new Error("User not found");
+
+        const photoUrl = await cloudinary.uploader.upload(photo);
+
+        const newProperty = await Property.create({
+            title,
+            description,
+            propertyType,
+            location,
+            price,
+            photo: photoUrl.url,
+            creator: user._id,
+        });
+
+        user.allProperties.push(newProperty._id);
+        await user.save({ session });
+
+        await session.commitTransaction();
+
+        res.status(200).json({ message: "Property created successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
+
 
 
 const updateProperty = async (req, res) => { };
